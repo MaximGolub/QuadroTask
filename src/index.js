dcf0572a1fa5b9ad9dd0586ef7ba43e07ec453c7
MaximGolub <maks.golub2@gmail.com>
2022-09-28 22:22:38 +0300

    A new feature drawDetail()

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ let detail = {
   lt: { radius: 0 },
   lb: { radius: 0 },
   rt: { radius: 50 },
-  rb: { radius: 0 }
+  rb: { radius: 0 },
 };
 
 {
@@ -15,8 +15,43 @@ let detail = {
   const widthInput = document.getElementById("widthInput");
   const leftAngleInput = document.getElementById("leftAngleInput");
   const rotateZ = document.getElementById("rotateZ");
+  const draw = document.getElementById("draw");
 
-  //your code here....
-  
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+
+  canvas.width = 400;
+  canvas.height = 400;
+
+  function drawDetail() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    if (
+      heightInput.value === "" ||
+      widthInput.value === "" ||
+      rightAngleInput.value === ""
+    ) {
+      ctx.beginPath();
+      ctx.moveTo(0, 0);
+      ctx.lineTo(0, detail.width);
+      ctx.lineTo(detail.length, detail.width);
+      ctx.arcTo(detail.length, 0, 0, 0, detail.rt.radius);
+      ctx.lineTo(0, 0);
+      ctx.stroke();
+      ctx.closePath();
+    } else {
+      ctx.beginPath();
+      ctx.moveTo(0, 0);
+      ctx.lineTo(0, widthInput.value);
+      ctx.lineTo(heightInput.value, widthInput.value);
+      ctx.arcTo(heightInput.value, 0, 0, 0, rightAngleInput.value);
+      ctx.lineTo(0, 0);
+      ctx.stroke();
+      ctx.closePath();
+    }
+
+    drawDetail();
+
+    draw.addEventListener("click", drawDetail);
+  }
 }
-
